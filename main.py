e31d29ac9ffb2dd528d50dc0e7e77043b189b1da
FreakGriffith <arte.gurin@gmail.com>
2026-01-03 22:16:28 +0100

    Add player and circle shape classes with drawing functionality

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
     VERSION = pygame.version.ver
     pygame.init()
-    
+
+    clock = pygame.time.Clock()
+    dt = 0
+    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     print(f"Starting Asteroids with pygame version: {VERSION}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -20,7 +25,11 @@ def main():
 
         log_state()
         screen.fill("black")
+        player_1.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+    
 
 if __name__ == "__main__":
     main()
