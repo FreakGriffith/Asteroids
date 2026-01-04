dd18f048b219085adb99502175ed4fd5f2b97533
FreakGriffith <arte.gurin@gmail.com>
2026-01-04 02:17:32 +0100

    Implement asteroid spawning and movement mechanics; enhance player controls

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import pygame
+
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     VERSION = pygame.version.ver
     pygame.init()
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+
     clock = pygame.time.Clock()
     dt = 0
     player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = AsteroidField()
 
     print(f"Starting Asteroids with pygame version: {VERSION}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -24,8 +37,11 @@ def main():
                 return
 
         log_state()
+
+        updatable.update(dt)
         screen.fill("black")
-        player_1.draw(screen)
+        for i in drawable:
+            i.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
